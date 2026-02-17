6a3f2ae374a424238071447cd9c6921c87fd34f7
Samadhan52 <you@example.com>
2026-02-17 09:55:57 -0500

    implemented pictures service

--- backend/routes.py
+++ backend/routes.py
@@ -7,11 +7,9 @@ SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
 json_url = os.path.join(SITE_ROOT, "data", "pictures.json")
 data: list = json.load(open(json_url))
 
-
 ######################################################################
 # RETURN HEALTH OF THE APP
 ######################################################################
-
 
 @app.route("/health")
 def health():
